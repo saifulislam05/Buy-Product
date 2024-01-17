4a5b829287a1f651cda3aa0fae4fc8626d7956e4
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-17 08:59:22 +0530

    project done till order summary

--- src/components/Cart/OrderSummary.js
+++ src/components/Cart/OrderSummary.js
@@ -1,25 +1,37 @@
 import React from "react";
 
-const OrderSummary = ({ totalItems, totalPrice }) => {
+const OrderSummary = ({ data }) => {
+  const totalQuantity = data.reduce((total, item) => {
+    return total + item.qty;
+  }, 0);
+
+  const totalPrice = data.reduce((total, item) => {
+    const discountedPrice =
+      item.price - (item.price * item.discountPercentage) / 100;
+    return total + discountedPrice * item.qty;
+  }, 0);
   return (
     <div id="summary" className="w-full px-8 py-10">
-      <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
-      <div className="flex justify-between mt-10 mb-5">
-        <span className="font-semibold text-sm uppercase">{`Items ${totalItems}`}</span>
-        <span className="font-semibold text-sm">{`$${totalPrice}`}</span>
+      <h1 className="font-semibold text-2xl border-b pb-4">Order Summary</h1>
+      <div className="flex justify-between my-5">
+        <span className="font-semibold text-sm uppercase">{`Items ${totalQuantity}`}</span>
+        <span className="font-semibold text-sm">{`$${totalPrice.toFixed(
+          2
+        )}`}</span>
       </div>
       <div>
-        <label className="font-medium inline-block mb-3 text-sm uppercase">
+        <label className="font-medium inline-block mb-2 text-sm uppercase">
           Shipping
         </label>
-        <select className="block p-2 text-gray-600 w-full text-sm">
+        <select className="select select-sm select-bordered outline-none w-full">
           <option>Standard shipping - $10.00</option>
+          <option>Normal shipping - $2.00</option>
         </select>
       </div>
-      <div className="py-10">
+      <div className="py-5">
         <label
           htmlFor="promo"
-          className="font-semibold inline-block mb-3 text-sm uppercase"
+          className="font-semibold inline-block mb-2 text-sm uppercase"
         >
           Promo Code
         </label>
@@ -27,18 +39,16 @@ const OrderSummary = ({ totalItems, totalPrice }) => {
           type="text"
           id="promo"
           placeholder="Enter your code"
-          className="p-2 text-sm w-full"
+          className="input outline-none input-bordered input-sm text-sm w-full"
         />
       </div>
-      <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
-        Apply
-      </button>
+      <button className="btn btn-error  btn-sm  uppercase">Apply</button>
       <div className="border-t mt-8">
         <div className="flex font-semibold justify-between py-6 text-sm uppercase">
           <span>Total cost</span>
           <span>{`$${totalPrice}`}</span>
         </div>
-        <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+        <button className="btn btn-primary uppercase font-bold tracking-widest w-full">
           Checkout
         </button>
       </div>
